681329c7ab8270e5d7e386397a8869b00a966d4a
Neil Okamoto <neil.okamoto@gmail.com>
2020-01-08 11:27:38 -0800

    write docstrings for posterity

--- src/poisson/core.clj
+++ src/poisson/core.clj
@@ -3,7 +3,9 @@
             [quil.core :as q]
             [quil.middleware :as m]))
 
-(defn setup []
+(defn setup
+  "Provide the initial state map for this applet"
+  []
   (q/frame-rate 15)
   {:kdtree (k/build-tree [])
    :building true
@@ -15,41 +17,60 @@
    :smax 50
    :offscreen false})
 
-(defn setup-offscreen []
-  (println "setup-offscreen")
+(defn setup-offscreen
+  "Initial state vector with :offscreen flag set true"
+  []
   (assoc (setup) :offscreen true))
 
-(defn settings []
+(defn settings
+  "Processing documentation insists this must be done here"
+  []
   (q/pixel-density 2)
   (q/smooth 16))
 
-(defn random-disc [{:keys [smin slim]}]
+(defn random-disc
+  "Randomly generate a disc's position and radius.
+  Return a map with keys :x :y :r"
+  [{:keys [smin slim]}]
   {:x (q/random 0 (q/width))
    :y (q/random 0 (q/height))
    :r (q/random smin slim)})
 
-(defn sq-dist [x1 y1 x2 y2]
+(defn sq-dist
+  "Compute the squared distance from (x1,y1) to (x2,y2)"
+  [x1 y1 x2 y2]
   (let [dx (- x1 x2)
         dy (- y1 y2)]
     (+ (* dx dx) (* dy dy))))
 
-(defn collision? [{:keys [x y r]}]
+(defn collision?
+  "Return a closure over the supplied disc, where the generated function
+  serves as a predicate to test points for collision with this disc."
+  [{:keys [x y r]}]
   (fn [p]
     (let [[px py] p
           pr (:r (meta p))]
       (< (sq-dist x y px py)
          (* (+ pr r) (+ pr r))))))
 
-(defn make-interval [{:keys [x y]} radius]
+(defn make-interval
+  "Make an square interval around the position of the supplied disc"
+  [{:keys [x y]} radius]
   [[(- x radius) (+ x radius)]
    [(- y radius) (+ y radius)]])
 
-(defn choose-color [{:keys [cmin cmax]}]
+(defn random-color
+  "Generate a random rgb triple whose channel values lie in the interval
+  from :cmin to :cmax"
+  [{:keys [cmin cmax]}]
   [(q/random cmin cmax)
    (q/random cmin cmax)
    (q/random cmin cmax)])
 
-(defn update-state [{:keys [kdtree building smin slim smax max-tries], :as state}]
+(defn update-state
+  "Update the state, mainly by generating a new disc in the display area
+  that doesn't intersect any existing discs."
+  [{:keys [kdtree building smin slim smax max-tries], :as state}]
   (if-not building
     state
     (loop [i 0]
@@ -69,13 +90,20 @@
                                            {:r (:r disc)
                                             :color (choose-color state)}))))))))
 
-(defn update-offscreen [{:keys [building slim], :as state}]
-  (q/print-every-n-millisec 1000 (format "update-offscreen building: %s lim: %s" building slim))
+(defn update-offscreen
+  "For offscreen rendering we recursively call `update-state` until all
+  discs are generated"
+  [{:keys [building slim], :as state}]
   (if building
-    (recur (update-state state))
+    (do
+      (->> (format "update-offscreen building: %s lim: %s" building slim)
+           (q/print-every-n-millisec 1000))
+      (recur (update-state state)))
     state))
 
-(defn draw-state [{:keys [kdtree building slim offscreen]}]
+(defn draw-state
+  "Draw all discs"
+  [{:keys [kdtree building slim offscreen]}]
   (q/background 24)
   (q/stroke-weight 0)
   (doseq [p (k/interval-search kdtree [[0 (q/width)] [0 (q/height)]])]
@@ -88,7 +116,9 @@
       (q/fill 255 255 255)
       (q/text (format "active: %s radius: %s" building slim) 10 20))))
 
-(defn draw-offscreen [state]
+(defn draw-offscreen
+  "Generate offscreen rendering context, then render, save, and exit"
+  [state]
   (println "draw-offscreen: begin")
   (let [gr (q/create-graphics (q/width) (q/height)
                               :pdf "generated/create-graphics.pdf")]
@@ -100,7 +130,7 @@
   (q/exit)
   (println "draw-offscreen: done"))
 
-(q/defsketch poisson                  ; offscreen pdf
+(q/defsketch poisson                    ; offscreen pdf
   :title "packed circles"
   :size [1200 600]
   :setup setup-offscreen
